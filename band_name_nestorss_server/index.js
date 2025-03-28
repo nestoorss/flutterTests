3c23be26048abadc45849301a2c2555bb79c2a73
nestorss05 <nestor.sanchez@iesnervion.es>
2025-03-28 14:45:34 +0100

    Servidor NODE basico

---
+++ band_name_nestorss_server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+// App de Express
+const app = express();
+
+// Node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+// Path publico
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+const PORT = process.env.PORT || 3000;
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error(err);
+    console.log(`Servidor corriendo en puerto:${PORT}`);
+});
